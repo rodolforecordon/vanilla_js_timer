be7b276c61b2f9eb9d71fd8fdc8a5b8afcb0e26e
rodolforecordon <rodolforecordon@gmail.com>
2021-09-20 20:53:54 -0300

    single event listener for all buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,54 @@
-const timer = document.querySelector('#timer');
-const start = document.querySelector('#start');
-const pause = document.querySelector('#pause');
-const reset = document.querySelector('#reset');
+function indexTimer() {
 
-let startTimer;
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
-let millisecs = 0;
+  const timer = document.querySelector('#timer');
 
-start.addEventListener('click', (event) => {
-  timer.classList.remove('paused');
+  let startTimer;
+  let hours = 0;
+  let minutes = 0;
+  let seconds = 0;
+  let millisecs = 0;
 
-  if (startTimer) clearInterval(startTimer);
-
-  startTimer = setInterval(() => {
-    if (millisecs !== 99) millisecs += 1;
-    if (millisecs === 99) {
-      millisecs = 0;
-      seconds += 1
+  document.addEventListener('click', (e) => {
+    const el = e.target;
+    
+    if (el.classList.contains('start')) {
+      timer.classList.remove('paused');
+      if (startTimer) clearInterval(startTimer);
+      
+      startTimer = setInterval(() => {
+        if (millisecs !== 99) millisecs += 1;
+        if (millisecs === 99) {
+          millisecs = 0;
+          seconds += 1
+        }
+        if (seconds === 59) {
+          seconds = 0;
+          minutes += 1;
+        }
+        if (minutes === 59) {
+          minutes = 0;
+          hours += 1;
+        }
+        timer.innerHTML = `${leftZero(hours)}:${leftZero(minutes)}:${leftZero(seconds)}:${leftZero(millisecs)}`;
+      }, 10);
     }
-    if (seconds === 59) {
-      seconds = 0;
-      minutes += 1;
-    }
-    if (minutes === 59) {
-      minutes = 0;
-      hours += 1;
+
+    if (el.classList.contains('pause')) {
+      timer.classList.add('paused');
+      clearInterval(startTimer);
     }
 
-    timer.innerHTML = `${leftZero(hours)}:${leftZero(minutes)}:${leftZero(seconds)}:${leftZero(millisecs)}`;
-  }, 1);
-});
+    if (el.classList.contains('reset')) {
+      [hours, minutes, seconds, millisecs] = [0, 0, 0, 0];
+      timer.classList.remove('paused');
+      timer.innerHTML = `${leftZero(hours)}:${leftZero(minutes)}:${leftZero(seconds)}:${leftZero(millisecs)}`;
+    }
+  })
 
-pause.addEventListener('click', (event) => {
-  timer.classList.add('paused');
-  clearInterval(startTimer);
-});
+  function leftZero(num) {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
 
-reset.addEventListener('click', (event) => {
-  [hours, minutes, seconds, millisecs] = [0, 0, 0, 0];
-  timer.classList.remove('paused');
-  timer.innerHTML = `${leftZero(hours)}:${leftZero(minutes)}:${leftZero(seconds)}:${leftZero(millisecs)}`;
-});
-
-function leftZero(num) {
-  return num < 10 ? `0${num}` : `${num}`;
 }
+
+indexTimer();
